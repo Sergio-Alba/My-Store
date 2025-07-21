27bef86102c44afe0cab966a0fe9d43528a0e86f
Sergio Alba <sergioalba.1099@gmail.com>
2025-07-21 15:16:19 -0400

    feat: Add Spinner component for loading indication

---
+++ src/shared/components/Spinner.tsx
@@ -0,0 +1,8 @@
+
+export const Spinner = () => {
+  return (
+    <div className='w-16 mx-auto' >
+      <div className="w-12 h-12 rounded-full border-8 border-white border-t-sky-500 animate-spinner-ease-spin "></div>
+    </div>
+  )
+}
