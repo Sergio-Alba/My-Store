6e0ff82c16a09e92829848b426dcae1293d0ccdd
Sergio Alba <sergioalba.1099@gmail.com>
2025-07-30 20:18:40 -0400

    feat: add optimistic update handling in useProductMutation hook

--- src/products/hooks/useProductMutation.ts
+++ src/products/hooks/useProductMutation.ts
@@ -7,13 +7,38 @@ export const useProductMutation = () => {
   const queryClient = useQueryClient();
   const mutation = useMutation({
       mutationFn: createProduct,
+
+      onMutate: (product) => {
+        console.log("Mutando - Optimistic Update", product);
+        //? Optimistically update the cache
+
+        //Optimistic product
+        const optimisticProduct = {
+          id: Math.random(),
+          ...product
+        }
+        // Administrar el producto en el cache del query client
+        queryClient.setQueryData<Product[]>(
+          ["products", {filterKey: product.category} ],
+          (old) => {
+            if(!old ) return [optimisticProduct];
+            
+            return [...old, optimisticProduct]
+          }
+        )
+        return { optimisticProduct }
+      },
+
+
+
+
       onSuccess:(data) => {
 
         //? Invalidate the products query to refetch the data
         // queryClient.invalidateQueries({
         //   queryKey: ["products",{"filterKey": data.category}]
         // })
-
+        
         queryClient.setQueryData<Product[]>(
           ["products",{"filterKey": data.category}],(oldProducts) => {
           if (!oldProducts) return [data]
